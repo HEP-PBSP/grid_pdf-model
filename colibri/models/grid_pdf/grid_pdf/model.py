40669359975cec422a57fa6c8b5cb3aa53d0060d
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-04-27 11:49:37 +0100

    Refactor prediction and PDF function in GridPDFModel

--- colibri/models/grid_pdf/grid_pdf/model.py
+++ colibri/models/grid_pdf/grid_pdf/model.py
@@ -78,18 +78,19 @@ class GridPDFModel(PDFModel):
 
         return interp_func
 
-    def predictions_func(self, interpolation_grid, pred_data):
-        """This function should produce a predictions function, which takes
-        in the model parameters, and produces the predictions for the data.
+    def pred_and_pdf_func(self, interpolation_grid, pred_data):
+        """This function should produce a function that takes in the model
+        parameters, and produces the predictions for the data, as well as the
+        PDF values on the grid.
         """
 
         @jax.jit
-        def pred(params):
+        def pred_and_pdf(params):
             pdf = self.grid_values_func(interpolation_grid)(params)
             predictions = pred_data(pdf)
-            return predictions
+            return predictions, pdf
 
-        return pred
+        return pred_and_pdf
 
 
 def mc_initial_parameters(pdf_model, mc_initialiser_settings, replica_index):
