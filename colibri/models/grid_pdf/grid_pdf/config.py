5a9a9528c759e444780476ed4405397c6f5d4d0b
Luca Mantani <luca.mantani@gmail.com>
2024-04-29 15:13:35 +0100

    Refactor prior in grid_pdf

--- colibri/models/grid_pdf/grid_pdf/config.py
+++ colibri/models/grid_pdf/grid_pdf/config.py
@@ -21,10 +21,6 @@ class GridPdfConfig(colibriConfig):
         return grid_pdf_settings["xgrids"]
 
     def parse_prior_settings(self, settings):
-        # Currently, all possible prior choices require a central PDF.
-        if "pdf_prior" not in settings.keys():
-            raise ValueError("Missing key pdf_prior for uniform_pdf_prior")
-
         # In the case of a uniform prior around a central PDF, we also need
         # to specify the total number of standard deviations around the mean
         # which we allow.
--- colibri/models/grid_pdf/grid_pdf/utils.py
+++ colibri/models/grid_pdf/grid_pdf/utils.py
@@ -6,9 +6,13 @@ Module containing util functions for grid PDF fits.
 
 from validphys import convolution
 
+import jax
 import jax.numpy as jnp
 
 import colibri
+from validphys.core import PDF
+
+import colibri.bayes_prior
 
 
 def closure_test_central_pdf_grid(
@@ -71,3 +75,107 @@ def closure_test_central_pdf_grid(
     reduced_pdfgrid = interpolator(parameters)
 
     return reduced_pdfgrid
+
+
+def pdf_prior_grid(prior_settings, pdf_model):
+    """
+    Load the replicas grid for the Bayesian prior.
+    """
+    # Load the prior PDF
+    pdf = PDF(prior_settings["pdf_prior"])
+
+    replicas_grid = jnp.concatenate(
+        [
+            convolution.evolution.grid_values(
+                pdf, [flavour], pdf_model.xgrids[flavour], [1.65]
+            )
+            for flavour in pdf_model.fitted_flavours
+        ],
+        axis=2,
+    )
+
+    return replicas_grid
+
+
+def bayesian_prior(prior_settings, pdf_model):
+    """
+    Produces the Bayesian prior for a grid_pdf fit. The options for the
+    prior are given in prior_settings, which is a dictionary with required
+    key 'type'. The 'type' is one of 'uniform_pdf_prior', 'gaussian_pdf_prior'.
+
+    NOTE: this function overridest the one in colibri.bayes_prior.
+
+    Parameters
+    ----------
+    pdf_model: pdf_model.PDFModel
+        The PDF model to fit.
+
+    prior_settings: dict
+        Settings for the prior.
+
+    Returns
+    -------
+    jit compiled function
+        The prior transform function.
+    """
+
+    if prior_settings["type"] == "uniform_pdf_prior":
+        nsigma = prior_settings["nsigma"]
+        pdf_grid = pdf_prior_grid(prior_settings, pdf_model)
+
+        # Remove central replica
+        replicas_grid = pdf_grid[1:, :, :, :]
+
+        error68_up = jnp.nanpercentile(replicas_grid, 84.13, axis=0).reshape(-1)
+        error68_down = jnp.nanpercentile(replicas_grid, 15.87, axis=0).reshape(-1)
+
+        # Compute the uncertainty on the central grid
+        delta = (error68_up - error68_down) / 2
+        mean = (error68_up + error68_down) / 2
+        error_up = mean + delta * nsigma
+        error_down = mean - delta * nsigma
+
+        @jax.jit
+        def prior_transform(cube):
+            params = error_down + (error_up - error_down) * cube
+            return params
+
+    elif prior_settings["type"] == "gaussian_pdf_prior":
+        pdf_grid = pdf_prior_grid(prior_settings, pdf_model)
+
+        central_prior_grid = pdf_grid[0, :, :, :].squeeze()
+        # Remove central replica
+        replicas_grid = pdf_grid[1:, :, :, :]
+
+        pdf_covmat_prior = jnp.cov(
+            replicas_grid.reshape((replicas_grid.shape[0], -1)).T
+        )
+
+        # note: this matrix could be ill-conditioned when too many xgrids are used
+        # this is because xgrid values are not independent
+        # in order to avoid this we only keep the diagonal of the covariance matrix
+        # and regularise it by cutting off small values
+        pdf_diag_covmat_prior = jnp.where(
+            jnp.diag(pdf_covmat_prior) < 0, 0, jnp.diag(pdf_covmat_prior)
+        )
+        cholesky_pdf_covmat = jnp.diag(jnp.sqrt(pdf_diag_covmat_prior))
+
+        @jax.jit
+        def prior_transform(cube):
+            """
+            This currently does not support vectorisation.
+            """
+            # generate independent gaussian with mean 0 and std 1
+            independent_gaussian = jax.scipy.stats.norm.ppf(cube)[:, jnp.newaxis]
+
+            # generate random samples from a multivariate normal distribution
+            prior = central_prior_grid + jnp.einsum(
+                "ij,kj->ki", independent_gaussian.T, cholesky_pdf_covmat
+            ).squeeze(-1)
+
+            return prior
+
+    else:
+        return colibri.bayes_prior.bayesian_prior(prior_settings)
+
+    return prior_transform
